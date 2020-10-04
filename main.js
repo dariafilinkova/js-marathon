39346faf5bab9f1530f82b2f58a4a124ce331ebc
dariafilinkova <daria.filinkova@gmail.com>
2020-10-04 20:38:36 +0300

    Homework 6

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const $btn1 = $getElById('btn-kick1');
 const $btn2 = $getElById('btn-kick2');
+const $btn3 = $getElById('btn-kick3');
+
 function $getElById(id) {
     return document.getElementById(id);
 }
+
 const character = {
     name: 'Pikachu',
-    defaultHP: 100,
-    damageHP: 100,
+    defaultHP: 200,
+    damageHP: 200,
     elHP: $getElById('health-character'),
     elProgressbar: $getElById('progressbar-character'),
     changeHP,
@@ -15,12 +18,10 @@ const character = {
     renderProgressbarHP,
 }
 
-
-
 const enemy = {
     name: 'Charmander',
-    defaultHP: 100,
-    damageHP: 100,
+    defaultHP: 200,
+    damageHP: 200,
     elHP: $getElById('health-enemy'),
     elProgressbar: $getElById('progressbar-enemy'),
     changeHP,
@@ -34,56 +35,30 @@ const enemy = {
 const { name: nameCharacter } = character;
 const { name: nameEnemy } = enemy;
 
-$btn1.addEventListener('click', function () {
+/*$btn1.addEventListener('click', function () {
     console.log('Kick');
     character.changeHP(random(20));
     enemy.changeHP(random(20));
-})
+})*/
+
+$btn1.addEventListener('click', generateFunctionOfDamage($btn1));
 
 
-function compareTheNumbers(userNumber, randomNumber, attempt) {
-    if ((attempt == 2 || attempt == 3) && userNumber > randomNumber) {
-        alert('Перебор.Повторите попытку\n');
-    }
-
-    if ((attempt == 2 | attempt == 3) && userNumber < randomNumber) {
-        alert('Недобор.Повторите попытку\n');
-    }
-    if (attempt == 4 && (userNumber > randomNumber || userNumber < randomNumber)) {
-        alert('Вы не угадали число (');
-    }
-}
-
-function guessTheNumber() {
-    let randomNumber = (random(10));
-    console.log(randomNumber);
-    let count = 0;
-    let attempt = 1;
-    while (count < 3) {
-
-        let userNumber = prompt(`Введите число от 1 до 10.\n Попытка № ${attempt} `, 1);
-        attempt++;
-        console.log(userNumber);
-        if (userNumber == randomNumber) {
-            alert('Вы угадали число!\nВы нанесли супер удар!');
-            enemy.changeHP(random(20));
-            break;
-        }
-        else
-            compareTheNumbers(userNumber, randomNumber, attempt);
-        count++;
-    }
-
-
-
-}
-
-$btn2.addEventListener('click', function () {
+/*$btn2.addEventListener('click', function () {
     console.log('Super hiiit');
-    alert('Чтобы воспользоваться супер ударом, угадайте число от 1 до 10');
-    guessTheNumber();
-    console.log('Super Hiiiit');
-})
+    enemy.changeHP(random(10) + 20);
+    //console.log('Super Hiiiit');
+})*/
+
+$btn2.addEventListener('click', generateFunctionOfDamage($btn2));
+
+/*$btn3.addEventListener('click', function () {
+    console.log('Elbow Blow Kick');
+    enemy.changeHP(random(12));
+    //console.log('Super Hiiiit');
+})*/
+
+$btn3.addEventListener('click', generateFunctionOfDamage($btn3));
 
 function init() {
     console.log('Start Game');
@@ -101,7 +76,7 @@ function renderHPLife() {
 }
 
 function renderProgressbarHP() {
-    this.elProgressbar.style.width = this.damageHP + '%';
+    this.elProgressbar.style.width = this.damageHP * 100 / this.defaultHP + '%';
 }
 
 function changeHP(count) {
@@ -109,26 +84,32 @@ function changeHP(count) {
     const currentHp = this.damageHP;
     const initialHP = this.defaultHP;
     const log = this === enemy ? generateLog(this, character, (count * (-1)), currentHp, initialHP) : generateLog(this, enemy, (count * (-1)), currentHp, initialHP);
-
     console.log(log);
-    const $logs=document.querySelector('#logs');
-    const $p = document.createElement('p');
-    $p.innerText=log;
-   
-    $logs.insertBefore($p,logs.children[0]);
+    printLog(log);
+
     if (this.damageHP <= 0) {
         this.damageHP = 0;
         alert(' Бедный ' + this.name + ' проиграл бой ( ');
         $btn1.disabled = true;
         $btn2.disabled = true;
+        $btn3.disabled = true;
     }
 
     this.renderHP();
 }
 
-function random(num) {
-    return Math.ceil(Math.random() * num);
+function printLog(log) {
+    const $paragraph = document.createElement('p');
+    $paragraph.innerText = log;
+    const $logs = document.querySelector('#logs');
+    $logs.insertBefore($paragraph, $logs.children[0])
 }
+
+/*function random(num) {
+    return Math.ceil(Math.random() * num);
+}*/
+
+const random = (num) => Math.ceil(Math.random() * num); //вместо function random(num) Это arrow function(стрелочная функция)
 
 function generateLog(firstPerson, secondPerson, count, currentHp, initialHP) {
     const logs = [
@@ -146,13 +127,53 @@ function generateLog(firstPerson, secondPerson, count, currentHp, initialHP) {
     return logs[random(logs.length) - 1];
 }
 
+function generateFunctionOfDamage($btn) {
+    let currentCount = 0;
+    if ($btn === $btn1) {
 
+        let maxCount = 10;
+        let $counter = $btn.querySelector("small span");
+        $counter.innerText = maxCount;
+        return function () {
+            character.changeHP(random(20));
+            enemy.changeHP(random(20));
+            currentCount++;
+            if (currentCount == maxCount)
+                this.disabled = true;
+            console.log(`Ты использовал ${this.children[0].innerText} ${currentCount} раз из ${maxCount}`);
+            $counter.innerText = maxCount - currentCount;
+            console.log('Kick!');
+        }
+    }
+    if ($btn === $btn2) {
+        let maxCount = 1;
+        let $counter = $btn.querySelector("small span");
+        $counter.innerText = maxCount;
+        return function () {
+            enemy.changeHP(random(10) + 20);
+            currentCount++;
+            if (currentCount == maxCount)
+                this.disabled = true;
+            console.log(`Ты использовал ${this.children[0].innerText} ${currentCount} раз из ${maxCount}`);
+            $counter.innerText = maxCount - currentCount;
+            console.log('Super Hiiiit');
+        }
 
-function showActions(){
-    const logs=document.querySelector('#logs');
-    const $p = document.createElement('p');
-    $p.innerText=log;
-   
-    $logs.insertBefore($p,logs.children[0]);
+    }
+    if ($btn === $btn3) {
+        let maxCount = 3;
+        let $counter = $btn.querySelector("small span");
+        $counter.innerText = maxCount;
+        return function () {
+            enemy.changeHP(random(12));
+            currentCount++;
+            if (currentCount == maxCount)
+                this.disabled = true;
+            console.log(`Ты использовал ${this.children[0].innerText} ${currentCount} раз из ${maxCount}`);
+            $counter.innerText = maxCount - currentCount;
+            console.log('Elbow Blow');
+        }
+    }
 }
+
 init();
